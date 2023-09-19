46cd5d12af705d47f44452f7b00d1eeebf25c614
Hieu <tranvanhieu20032001@gmail.com>
2023-09-19 17:29:26 +0700

    Setup basic

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft"></div>
+        <div className="topbarCenter"></div>
+        <div className="topbarRight"></div>
+    </div>
+  )
+}
